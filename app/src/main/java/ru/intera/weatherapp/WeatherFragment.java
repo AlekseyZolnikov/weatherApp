5fd01da6a1a4ccd303ba2757a66f6c72bcd8ad90
LSDance <monsarda@yandex.ru>
2019-04-13 10:36:01 +0300

    small fix

--- app/src/main/java/ru/intera/weatherapp/WeatherFragment.java
+++ app/src/main/java/ru/intera/weatherapp/WeatherFragment.java
@@ -6,13 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class WeatherFragment extends Fragment {
+
+    private TextView textView;
 
     public WeatherFragment() {
         // Required empty public constructor
@@ -24,6 +24,10 @@ public class WeatherFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View fragmentView = inflater.inflate(R.layout.fragment_weather, container, false);
+
+        textView = fragmentView.findViewById(R.id.weather_title_city);
+        String str = getArguments().getString(WeatherActivity.EXTRA_CITY_KEY);
+        textView.setText(str);
         return fragmentView;
     }
 
